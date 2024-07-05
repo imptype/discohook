4d84fd25d21e48fc7db0027514bd9ad0982a0d6f
imp <106537315+imptype@users.noreply.github.com>
2024-07-05 15:36:27 +0100

    create session in async context

--- discohook/client.py
+++ discohook/client.py
@@ -115,9 +115,7 @@ class Client(Starlette):
         self.public_key = public_key
         self.application_id = application_id
         self.password = password
-        self.http = HTTPClient(
-            self, token, aiohttp.ClientSession("https://discord.com")
-        )
+        self.http = HTTPClient(self, token)
         self.active_components: Dict[str, Component] = {}
         self._sync_queue: List[ApplicationCommand] = []
         self.commands: Dict[str, ApplicationCommand] = {}
